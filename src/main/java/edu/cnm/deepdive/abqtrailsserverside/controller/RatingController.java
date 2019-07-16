e87934a72f3cf01622e5a2cf777726e2841889a1
Alana Chigbrow <alana.chigbrow@gmail.com>
2019-07-16 14:37:40 -0600

    Fixed photo. I hope.

--- src/main/java/edu/cnm/deepdive/abqtrailsserverside/controller/RatingController.java
+++ src/main/java/edu/cnm/deepdive/abqtrailsserverside/controller/RatingController.java
@@ -1,7 +1,6 @@
 package edu.cnm.deepdive.abqtrailsserverside.controller;
 
 import edu.cnm.deepdive.abqtrailsserverside.model.dao.RatingRepository;
-import edu.cnm.deepdive.abqtrailsserverside.model.entity.Photo;
 import edu.cnm.deepdive.abqtrailsserverside.model.entity.Rating;
 import edu.cnm.deepdive.abqtrailsserverside.model.entity.Trail;
 import edu.cnm.deepdive.abqtrailsserverside.model.entity.User;
@@ -34,7 +33,6 @@ public class RatingController {
   public RatingController(RatingRepository repository) {
     this.repository = repository;
   }
-
 
   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
   public List<Rating> list() {
